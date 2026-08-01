07874f3770dea93f0feca37c60b8a87e746e1e04
AlemEsv <cinver.espinoza.v@uni.pe>
2026-08-01 14:32:00 -0500

    feat(frontend): integrate main navigation layout and updated routes in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,24 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
+import { AppLayout } from './layouts/AppLayout';
+import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LoginPage';
 import { ProfilePage } from './pages/ProfilePage';
 import { ScanPage } from './pages/ScanPage';
+import { VoiceMealLogPage } from './pages/VoiceMealLogPage';
+import { RecipeFromIngredientsPage } from './pages/RecipeFromIngredientsPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/perfil" replace />} />
-      <Route path="/perfil" element={<ProfilePage />} />
-      <Route path="/escanear" element={<ScanPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/perfil" element={<ProfilePage />} />
+        <Route path="/escanear" element={<ScanPage />} />
+      </Route>
+      <Route path="/contarme-que-comi" element={<VoiceMealLogPage />} />
+      <Route path="/que-puedo-cocinar" element={<RecipeFromIngredientsPage />} />
     </Routes>
   );
 }
